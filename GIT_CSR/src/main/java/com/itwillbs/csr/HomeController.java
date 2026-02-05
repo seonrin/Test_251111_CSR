097d13fa6404ce0a35fe8674145b14e0accac94a
CSR <ssunrinee@gmail.com>
2026-02-05 11:19:34 +0900

    aaa

--- GIT_CSR/src/main/java/com/itwillbs/csr/HomeController.java
+++ GIT_CSR/src/main/java/com/itwillbs/csr/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		// 테스트
+		// 테스트222222
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
